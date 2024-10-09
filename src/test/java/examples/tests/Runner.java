c20e6d70efbdf255ac486305f4592ca5d3775653
Adelyas <adelyas1705@gmail.com>
2024-10-10 00:21:36 +0700

    Practical Assessment 7

--- src/test/java/examples/tests/Runner.java
+++ src/test/java/examples/tests/Runner.java
@@ -4,11 +4,11 @@ import com.intuit.karate.junit5.Karate;
 
 public class Runner {
     @Karate.Test
-//    public Karate runPet()
-//    {return Karate.run("PetRequest").relativeTo(getClass());}
-//    @Karate.Test
-//    public Karate runUser()
-//    {return Karate.run("UserRequest").relativeTo(getClass());}
+    public Karate runPet()
+    {return Karate.run("PetRequest").relativeTo(getClass());}
+    @Karate.Test
+    public Karate runUser()
+    {return Karate.run("UserRequest").relativeTo(getClass());}
     public Karate runStore()
     {return Karate.run("StoreRequest").relativeTo(getClass());}
 }
